408d1668d709455a5f953df863c9e057a26bea9e
ahmed <ahmedaoudah1@gmail.com>
2025-08-01 22:17:19 +0300

    send to the gitddds

--- resources/views/books/client/read.blade.php
+++ resources/views/books/client/read.blade.php
@@ -1,14 +1,7 @@
     <div class="container mx-auto p-6">
         <h1 class="text-2xl font-bold mb-4">{{ $book->title }}</h1>
 
-        <div class="rounded-xl overflow-hidden shadow-lg border">
-            <iframe
-                src="{{ route('books.pdf', $book->id) }}"
-                width="100%"
-                height="800px"
-                class="w-full">
-            </iframe>
-        </div>
+
     </div>
 
     <div class="container mx-auto p-6 max-w-7xl ">
