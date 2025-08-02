bf3d5c420da9d10d8d3fe1f158a49c96107be6ff
ahmed <ahmedaoudah1@gmail.com>
2025-08-02 13:41:24 +0300

    /

--- resources/views/books/client/read.blade.php
+++ resources/views/books/client/read.blade.php
@@ -3,7 +3,7 @@
 
         <div class="rounded-xl overflow-hidden shadow-lg border">
             <iframe
-                src="{{ route('books.pdf', $book->id) }} #toolbar=0&download=false&print=false"
+                src="{{ route('books.pdf', $book->id) }}"
                 width="100%"
                 height="800px"
                 class="w-full">
