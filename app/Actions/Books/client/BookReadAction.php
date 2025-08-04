bb0b9b95623131afa319724c3b6f52f1e6cd9da3
ahmed <ahmedaoudah1@gmail.com>
2025-08-04 17:45:41 +0300

    /

--- app/Actions/Books/client/BookReadAction.php
+++ app/Actions/Books/client/BookReadAction.php
@@ -36,7 +36,7 @@ class BookReadAction
         }
 
 
-        $pdfContent = Storage::disk('public/storage/')->get($book->cover_url);
+        $pdfContent = Storage::disk('/public')->get($book->cover_url);
 
         return response($pdfContent)
             ->header('Content-Type', 'application/pdfs')
