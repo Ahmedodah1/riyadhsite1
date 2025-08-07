5ce461fe114f4d4c6d69649610e8df4afa87763a
ahmed <ahmedaoudah1@gmail.com>
2025-08-07 22:58:30 +0300

    /

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::prefix('auth')->group(function () {
 
 
 
-    Route::get('/Decor/index', \App\Actions\Decors\Admin\DecorAdminIndex::class)->name('Decor.index');
+    Route::get('/Decor/index', [\App\Actions\Decors\Admin\DecorAdminIndex::class, 'handle'])->name('Decor.index');
     Route::get('/Decor/create/view', \App\Actions\Decors\Admin\DecorCreateViewIndex::class)->name('Decor-create-view');
     Route::get('/Decor/edit/view/{decor}', \App\Actions\Decors\Admin\DecorEditView::class)->name('Decor-update-view');
 
