33e60823e34aea97a510731aa707ce8803a5e36b
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 12:52:07 +0300

    s

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,5 +1,5 @@
     <div class="container mx-auto p-6">
-        <h1 class="text-2xl font-bold mb-4 textt-center">{{ $certificate->title }}</h1>
+        <h1 class="text-2xl font-bold mb-4 text-center">{{ $certificate->title }}</h1>
 
         <div class="rounded-xl overflow-hidden shadow-lg border">
             <iframe
