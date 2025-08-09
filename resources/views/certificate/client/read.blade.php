0d23d1b3310a268b16076a764ea2b001b008a548
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 13:00:51 +0300

    update

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,5 +1,4 @@
-<div class="container mx-auto p-6">
-    <div class="w-full text-center">
+
         <h1 class="inline-block text-3xl font-bold mb-6 text-gray-800 tracking-wide">
             {{ $certificate->title }}
         </h1>
