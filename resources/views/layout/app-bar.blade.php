9e50e6f4762c15e03f35333f5442856d262ca81c
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 19:41:37 +0300

    update

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -1,6 +1,6 @@
 <nav id="nav" class="fixed left-0 w-full px-5 py-3 bg-transparent text-white transition-colors duration-300">
     <div class="flex justify-between items-center container mx-auto relative">
-        <a href="{{route('homepage')}}" class="flex items-center gap-3">
+        <a href="" class="flex items-center gap-3">
             <span id="logo-text" class="text-lg font-bold lg:text-3xl mt-1"></span>
             <img src="resha.png" alt="Logo" class=" w-30">
         </a>
@@ -13,59 +13,55 @@
 
         <div class=" {{!request()->is('/') ? 'text-black' : 'text-white'}} hidden lg:flex flex-col md:flex-row items-center justify-center gap-8 md:static absolute w-full md:w-auto p-4 md:p-0 z-20 top-full left-0 md:top-auto md:left-auto"
              :class="{'text-white': !scrolled, 'text-black': scrolled}">
-            <style>
-                .nav-link {
-                    @apply hover:no-underline transition-all duration-300 hover:text-gray-300 transform hover:scale-105;
-                }
-            </style>
-
             <div>
-                <a href="{{route('homepage')}}" class="nav-link">الرئيسية</a>
+                <a href="" class="hover:underline">الرئيسية</a>
             </div>
 
             <div class="relative group">
-                <a href="{{route('whous')}}" class="nav-link flex items-center space-x-1">
+                <a href="/whous" class="hover:underline flex items-center space-x-1">
                     <span>من آنا</span>
                 </a>
             </div>
 
             <div class="relative group">
-                <a href="{{route('books.index')}}" class="nav-link flex items-center">
-                    <span>{{ __('site.books') }}</span>
+                <a href="{{route('books.index')}}" class="hover:underline flex items-center">
+                    <span>{{ __('site.books') }}
+</span>
                 </a>
             </div>
 
             <div class="relative group">
-                <a href="#" class="nav-link flex items-center">
+                <a href="#" class="hover:underline flex items-center">
                     <span>تصويري </span>
                     <span class="material-icons transform group-hover:rotate-180 transition-transform">expand_more</span>
                 </a>
-                <div class="absolute left-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity z-10">
-                    <a href="{{route('ClientBlackAndWhite')}}" class="block px-4 py-2 hover:bg-gray-100 nav-link">صور آبيض وآسود</a>
-                    <a href="{{route('colorphotos')}}" class="block px-4 py-2 hover:bg-gray-100 nav-link">صور ملونه</a>
+                <div
+                    class="absolute left-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity z-10">
+                    <a href="{{route('ClientBlackAndWhite')}}" class="block px-4 py-2 hover:bg-gray-100">صور آبيض وآسود</a>
+                    <a href="{{route('colorphotos')}}" class="block px-4 py-2 hover:bg-gray-100">صور ملونه</a>
                 </div>
             </div>
 
             <div class="relative group">
-                <a href="{{route('decor')}}" class="nav-link flex items-center">
+                <a href="{{route('decor')}}" class="hover:underline flex items-center">
                     <span> صور الديكورات</span>
                 </a>
             </div>
 
             <div class="relative group">
-                <a href="{{route('paintings')}}" class="nav-link flex items-center">
+                <a href="{{route('paintings')}}" class="hover:underline flex items-center">
                     <span>معرض اللوحات</span>
                 </a>
             </div>
 
             <div class="relative group">
-                <a href="{{route('certificate')}}" class="nav-link flex items-center">
+                <a href="{{route('certificate')}}" class="hover:underline flex items-center">
                     <span>الشهادات والدورات</span>
                 </a>
             </div>
 
             <div class="relative group">
-                <a href="{{route('blog')}}" class="nav-link flex items-center">
+                <a href="{{route('blog')}}" class="hover:underline flex items-center">
                     <span>الإنجازات الهندسية</span>
                 </a>
             </div>
@@ -74,11 +70,10 @@
                 @if(\Illuminate\Support\Facades\Auth::check())
                     <form action="{{route('logout')}}" method="POST">
                         @csrf
-                        <button type="submit" class="nav-link">LogOut</button>
+                        <button type="submit" class="hover:underline">LogOut</button>
                     </form>
                 @endif
             </div>
-
         </div>
 
         <div class="group hidden lg:block">
@@ -90,50 +85,53 @@
 </nav>
 
 <div id="menu"
-     class=" top-0 left-0 w-full h-1/2 p-4 bg-black text-white shadow-lg z-30 hidden flex-col gap-9 items-start justify-start pt-10 lg:hidden overflow-y-auto">
+     class="fixed top-0 left-0 w-full h-1/2 p-4 bg-black text-white shadow-lg z-30 hidden flex-col gap-9 items-start justify-start pt-10 lg:hidden overflow-y-auto">
 
     <div class="mt-7">
-        <a href="{{ route('homepage') }}" class="hover:underline">الرئيسية</a>
+        <a href="" class="hover:underline">الرئيسية</a>
     </div>
-
-    <div>
-        <a href="{{ route('whous') }}" class="hover:underline">من آنا</a>
-    </div>
-
-
-
-
-
-
-    <div>
-        <a href="{{ route('books.index') }}" class="hover:underline">الكتب</a>
-    </div>
-
     <div class="relative group">
         <a href="#" class="hover:underline flex items-center space-x-1">
-            <span>تصويري</span>
-            <span class="material-icons transform group-hover:rotate-180 transition-transform">expand_more</span>
+            <span>من آنا</span>
         </a>
-        <div class="absolute right-0 mt-2 w-60 bg-white text-black rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50">
-            <a href="{{ route('ClientBlackAndWhite') }}" class="block px-4 py-2 hover:bg-gray-100">صور آبيض وآسود</a>
-            <a href="{{ route('colorphotos') }}" class="block px-4 py-2 hover:bg-gray-100">صور ملونه</a>
+        <div
+            class="absolute right-0 mt-2 w-40 bg-white text-black rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50">
+
         </div>
     </div>
+    <div class="relative group">
+        <a href="#" class="hover:underline flex items-center space-x-1">
+            <span>الكتب</span>
+        </a>
+        <div
+            class="absolute right-0 mt-2 w-40 bg-white text-black rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50">
 
+        </div>
+    </div>
+    <div class="relative group">
+        <a href="#" class="hover:underline flex items-center space-x-1">
+            <span> تصويري</span>
+            <span class="material-icons transform group-hover:rotate-180 transition-transform">expand_more</span>
+        </a>
+        <div
+            class="absolute right-0 mt-2 w-60 bg-white text-black rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 z-50">
+            <a href="" class="block px-4 py-2 hover:bg-gray-100">صور آسود وآبيض</a>
+            <a href="" class="block px-4 py-2 hover:bg-gray-100">صور ملونة</a>
+
+        </div>
+    </div>
     <div>
-        <a href="{{ route('decor') }}" class="hover:underline">صور الديكورات</a>
+        <a href="" class="hover:underline">صور الديكورات </a>
+    </div>
+    <div>
+        <a href="" class="hover:underline">معرض اللوحات</a>
     </div>
 
     <div>
-        <a href="{{ route('paintings') }}" class="hover:underline">معرض اللوحات</a>
+        <a href="" class="hover:underline">الشهادات والدورات</a>
     </div>
-
     <div>
-        <a href="{{ route('certificate') }}" class="hover:underline">الشهادات والدورات</a>
-    </div>
-
-    <div>
-        <a href="{{ route('blog') }}" class="hover:underline">الإنجازات الهندسية</a>
+        <a href="" class="hover:underline">الإنجازات الهندسية</a>
     </div>
 </div>
 
