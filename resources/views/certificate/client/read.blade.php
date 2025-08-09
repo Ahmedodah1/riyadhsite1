ea8df6980ecbcfe451f442f9b4a18314dcf8de21
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 12:48:09 +0300

    /update

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,4 +1,8 @@
-
+<div class="w-full p-6 flex justify-center">
+    <h1 class="text-3xl font-bold mb-6 text-gray-800 tracking-wide text-center">
+        {{ $certificate->title }}
+    </h1>
+</div>
 
 
 <div class="rounded-xl overflow-hidden shadow-lg border">
