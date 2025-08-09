f2302860b81de903f63ba700cc795d6f830664c6
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 12:57:46 +0300

    s

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,7 +1,9 @@
 <div class="container mx-auto p-6">
-    <h1 class="text-3xl font-bold mb-6 text-gray-800 tracking-wide text-right !block !text-center">
-        {{ $certificate->title }}
-    </h1>
+    <div class="w-full text-center">
+        <h1 class="inline-block text-3xl font-bold mb-6 text-gray-800 tracking-wide">
+            {{ $certificate->title }}
+        </h1>
+    </div>
 
     <div class="rounded-xl overflow-hidden shadow-lg border">
         <iframe
