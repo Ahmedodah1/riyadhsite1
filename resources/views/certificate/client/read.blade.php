90a7feb1eca123b2d63805a11782f9857c95c0ba
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 13:03:41 +0300

    update

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,7 +1,10 @@
-    <div class="container mx-auto p-6">
-        <h1 class="text-2xl font-bold mb-4 text-center">{{ $certificate->title }}</h1>
+<div class="container mx-auto p-6 max-w-7xl ">
+    <h1 style="text-align: center;" class="text-3xl font-extrabold mb-8 text-gray-900 dark:text-gray-100">
+        {{ $certificate->title }}
+    </h1>
 
-        <div class="rounded-xl overflow-hidden shadow-lg border">
+
+    <div class="rounded-xl overflow-hidden shadow-lg border">
             <iframe
                 src="{{ route('certificate.pdf', $certificate->id) }}"
                 width="100%"
@@ -11,3 +14,21 @@
         </div>
     </div>
 
+    <div class="container mx-auto p-6 max-w-7xl ">
+        <h1 style="text-align: center;" class="text-3xl font-extrabold mb-8 text-gray-900 dark:text-gray-100">
+            {{ $certificate->title }}
+        </h1>
+
+        <div
+            class="rounded-xl shadow-xl border border-gray-300 dark:border-gray-700 overflow-hidden"
+            style="height: 1000px;">
+
+            <iframe
+                src="{{ asset('pdfjs/web/viewer.html') }}?file={{ urlencode(route('certificate.pdf', $certificate->id)) }}#toolbar=0&download=false&print=false"
+                width="100%"
+                height="100%"
+                class="w-full h-full border-0"
+                sandbox="allow-scripts allow-same-origin allow-forms allow-popups">
+            </iframe>
+        </div>
+    </div>
