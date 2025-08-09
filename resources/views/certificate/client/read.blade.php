58352c0e203cccc1065c52e3243e65f3a445ec29
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 12:55:47 +0300

    s

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,14 +1,14 @@
-    <div class="container mx-auto p-6">
-        <h1 class="text-2xl font-bold mb-4">{{ $certificate->title }}</h1>
+<div class="container mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-6 text-gray-800 tracking-wide text-center !block !text-center">
+        {{ $certificate->title }}
+    </h1>
+
+    <div class="rounded-xl overflow-hidden shadow-lg border">
+        <iframe
+            src="{{ route('certificate.pdf', $certificate->id) }}"
+            width="100%"
+            height="800px"
+            class="w-full">
+        </iframe>
     </div>
-        <div class="rounded-xl overflow-hidden shadow-lg border">
-            <iframe
-                src="{{ route('certificate.pdf', $certificate->id) }}"
-                width="100%"
-                height="800px"
-                class="w-full">
-            </iframe>
-        </div>
-
-
-
+</div>
