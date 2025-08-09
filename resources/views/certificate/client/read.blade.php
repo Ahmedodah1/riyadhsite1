c861d9ab4893ac8abd8d38c81321d57bbd8b9fa8
ahmed <ahmedaoudah1@gmail.com>
2025-08-09 13:00:34 +0300

    update

--- resources/views/certificate/client/read.blade.php
+++ resources/views/certificate/client/read.blade.php
@@ -1,16 +1,8 @@
 <div class="container mx-auto p-6">
     <div class="w-full text-center">
-        <h1 class="inline-block text-3xl font-bold mb-6 text-gray-800 tracking-wide bg-red">
+        <h1 class="inline-block text-3xl font-bold mb-6 text-gray-800 tracking-wide">
             {{ $certificate->title }}
         </h1>
     </div>
 
-    <div class="rounded-xl overflow-hidden shadow-lg border">
-        <iframe
-            src="{{ route('certificate.pdf', $certificate->id) }}"
-            width="100%"
-            height="800px"
-            class="w-full">
-        </iframe>
-    </div>
-</div>
+
