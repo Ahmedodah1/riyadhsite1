546bc6f43cc43dd5465f7b72cd9a4f343eb82d52
ahmed <ahmedaoudah1@gmail.com>
2025-08-18 17:43:03 +0300

    update22

--- resources/views/coins/admin/edit.blade.php
+++ resources/views/coins/admin/edit.blade.php
@@ -17,6 +17,7 @@
             <div class="mb-4">
                 <label for="description" class="block mb-1 font-medium">وصف الصورة</label>
                 <input id="description" type="hidden" name="description" value="{{ $coin->description }}">
+                <trix-editor input="description" class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400" dir="rtl"></trix-editor>
             </div>
 
         @if($coin->cover_url)
