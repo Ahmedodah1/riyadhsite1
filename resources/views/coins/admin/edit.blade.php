72056788d6a61521d1c34cebe35dd69c8e1e337f
ahmed <ahmedaoudah1@gmail.com>
2025-08-18 17:44:19 +0300

    update22

--- resources/views/coins/admin/edit.blade.php
+++ resources/views/coins/admin/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.layout.dashboard')
 
 @section('content')
-
     <div class="max-w-2xl mx-auto mt-10 bg-white p-6 rounded shadow text-right">
         <h2 class="text-2xl font-bold mb-6">تعديل الصورة</h2>
 
@@ -16,9 +15,11 @@
 
             <div class="mb-4">
                 <label for="description" class="block mb-1 font-medium">وصف الصورة</label>
-                <input id="description" type="hidden" name="description" value="{{ $coin->description }}">
-                <trix-editor input="description" class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400" dir="rtl"></trix-editor>
+                <textarea dir="rtl" id="description" name="description" rows="4"
+                          class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                          required>{{ $coin->description }}</textarea>
             </div>
+
 
         @if($coin->cover_url)
                 <div class="mb-2">
