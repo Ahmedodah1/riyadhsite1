0b242a3d3c61616388d4b51de88bb91c852f0b4a
ahmed <ahmedaoudah1@gmail.com>
2025-08-18 20:16:58 +0300

    update22

--- resources/views/components/ClientBlackAndWhite/BlackAndWhite.blade.php
+++ resources/views/components/ClientBlackAndWhite/BlackAndWhite.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ open: false, image: '', visible: 8 }" class="relative">
     <!-- العنوان -->
     <div class="bg-gray-50 min-h-screen py-16 px-4 sm:px-10 lg:px-20">
-        <h1 class="text-3xl font-extrabold text-center text-gray-800 mt-20">📸 معرض الصور</h1>
+        <h1 class="text-3xl font-extrabold text-center text-gray-800 mt-20">تصويري  </h1>
 
         <!-- شبكة الصور -->
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-11">
