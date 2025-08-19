f4ca878009653cd847e47df7996a9354c8212bb5
ahmed <ahmedaoudah1@gmail.com>
2025-08-19 18:06:15 +0300

    update22

---
+++ app/Actions/Bathrooms/Client/BathroomClientIndex.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Actions\Bathrooms\Client;
+
+use App\Models\bathroom;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class BathroomClientIndex
+{
+    use AsAction;
+
+    public function handle()
+    {
+        $bathrooms = Bathroom::query()->get();
+        return view('bathroom.client.coin', compact('bathrooms'));
+    }
+}
