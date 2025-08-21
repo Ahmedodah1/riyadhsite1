3a4a9d0092e6cce664492d2a2810fbe98ef4d40b
ahmed <ahmedaoudah1@gmail.com>
2025-08-21 19:44:17 +0300

    update22

--- resources/views/coins/client/coin.blade.php
+++ resources/views/coins/client/coin.blade.php
@@ -12,7 +12,7 @@
                         x-show="{{ $index }} < visible"
                         class="bg-white rounded-2xl border border-gray-200 shadow hover:shadow-md transition duration-300 overflow-hidden"
                     >
-                        <button @click="open = true; image = @js(asset('storage/' . $coin->image))" class="block w-full">
+                        <button @click="open = true; image = @js(asset('public/storage/' . $coin->image))" class="block w-full">
                             <img src="{{ asset('public/storage/' . $coin->image) }}"
                                  alt="{{ $coin->title }}"
                                  class="w-full h-64 object-cover hover:scale-105 transition duration-300" />
