e0592e16641c0ca4fc897f12e11482a21110eabd
ahmed <ahmedaoudah1@gmail.com>
2025-08-22 02:24:25 +0300

    update22

--- app/Actions/Coins/Admin/EditCoinAction.php
+++ app/Actions/Coins/Admin/EditCoinAction.php
@@ -25,7 +25,7 @@ class EditCoinAction
             'title' => $request->title,
             'description' => $request->description,
             'country' => $request->country,
-            'related' => json_encode($request->related ?? []), // تحويل array إلى JSON
+            'related' => $request->related ?? [], // حفظ العملات المشابهة
         ];
 
         if ($request->hasFile('image')) {
