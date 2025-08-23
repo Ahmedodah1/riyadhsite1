e4e9655c0c08dc85aef32a8ffb3e3b8df607a39e
ahmed <ahmedaoudah1@gmail.com>
2025-08-23 21:58:12 +0300

    q

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -297,16 +297,23 @@
             color: black !important;
         }
 
-        .nav-link {
-            @apply transition-all duration-300 ease-in-out;
-        }
+    .nav-link {
+        @apply transition-all duration-300 ease-in-out;
+    }
 
+        /* عند مرور الماوس */
         .nav-link:hover {
-            color: #3b82f6; /* أزرق Tailwind */
+            color: #3b82f6; /* Tailwind Blue */
             transform: scale(1.1);
             text-decoration: none;
         }
-    }
+
+        /* عند الضغط بالماوس */
+        .nav-link:active {
+            transform: scale(0.95); /* ينكمش شوي */
+        }
+
+}
 
     nav.navbar-scrolled a,
     nav.navbar-scrolled span,
