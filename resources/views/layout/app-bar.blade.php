673624a2b4e89fd223edb49738c1b9f2de06687e
ahmed <ahmedaoudah1@gmail.com>
2025-08-23 13:19:03 +0300

    update22

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -34,17 +34,46 @@
                 <a href="{{route('homepage')}}" class="nav-link">الرئيسية</a>
             </div>
 
+
+
+
+
+
+
             <div class="relative group">
                 <a href="/whous" class="nav-link flex items-center space-x-1">
                     <span>من آنا</span>
                 </a>
             </div>
 
+
+
+
+
+
+
             <div class="relative group">
                 <a href="{{route('books.index')}}" class="nav-link flex items-center">
                     <span>{{ __('site.books') }}</span>
                 </a>
             </div>
+
+
+
+
+
+            <div class="relative group">
+                <a href="#" class="nav-link flex items-center">
+                    <span>تصويري </span>
+                    <span class="material-icons transform group-hover:rotate-180 transition-transform">expand_more</span>
+                </a>
+                <div class="absolute left-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity z-10">
+                    <a href="/whous" class="block px-4 py-2 hover:bg-gray-100 nav-link">من آنا</a>
+                    <a href="{{route('colorphotos')}}" class="block px-4 py-2 hover:bg-gray-100 nav-link">صور رسميه لي</a>
+                </div>
+            </div>
+
+
 
             <div class="relative group">
                 <a href="#" class="nav-link flex items-center">
