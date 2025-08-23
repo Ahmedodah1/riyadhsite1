c70fd897597978f339c0986222338c432f93530c
ahmed <ahmedaoudah1@gmail.com>
2025-08-23 22:22:25 +0300

    q

--- resources/views/layout/app-bar.blade.php
+++ resources/views/layout/app-bar.blade.php
@@ -38,22 +38,21 @@
 
 
 
-
             <div class="relative group">
                 <a href="#" class="nav-link flex items-center">
-                    <span>عني </span>
+                    <span>عني</span>
                     <span class="material-icons transform group-hover:rotate-180 transition-transform">
             expand_more
         </span>
                 </a>
 
                 <div class="absolute left-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg
-                hidden group-hover:block z-10">
+                opacity-0 scale-95 invisible group-hover:opacity-100 group-hover:scale-100
+                group-hover:visible transition-all duration-200 z-10">
                     <a href="/whous" class="block px-4 py-2 hover:bg-gray-100">من آنا</a>
                     <a href="{{route('colorphotos')}}" class="block px-4 py-2 hover:bg-gray-100">صور رسميه لي</a>
                 </div>
             </div>
-
 
 
 
