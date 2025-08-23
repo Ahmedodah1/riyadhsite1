853d2691b324faee3a7258a74d46255630d7b5c5
ahmed <ahmedaoudah1@gmail.com>
2025-08-23 14:29:58 +0300

    update22

--- app/Actions/SumBooks/Admin/SumBookAdminIndex.php
+++ app/Actions/SumBooks/Admin/SumBookAdminIndex.php
@@ -11,7 +11,7 @@ class SumBookAdminIndex
 
     public function handle()
     {
-        $sumbooks = SumBook::query()->get();
-        return view('SumBooks.admin.index' , compact('sumbooks'));
+        $sumbook = SumBook::query()->get();
+        return view('SumBooks.admin.index' , compact('sumbook'));
     }
 }
