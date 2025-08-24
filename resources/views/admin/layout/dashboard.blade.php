542e1db329b65d291c11ab5663fb5a827445a3ba
ahmed <ahmedaoudah1@gmail.com>
2025-08-24 21:33:17 +0300

    qw

--- resources/views/admin/layout/dashboard.blade.php
+++ resources/views/admin/layout/dashboard.blade.php
@@ -24,7 +24,8 @@
 </div>
 
 {{-- شاشة الترحيب تظهر فقط على صفحة Dashboard --}}
-@if($showWelcome)
+{{-- شاشة الترحيب تظهر فقط على صفحة Dashboard --}}
+@if(isset($showWelcome) && $showWelcome)
     <div id="welcomeModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
         <div class="bg-white rounded-2xl shadow-2xl p-8 max-w-md text-center animate-fade-in">
             <img src="{{ asset('resha.png') }}" alt="logo" class="w-20 mx-auto mb-4">
@@ -39,6 +40,7 @@
         </div>
     </div>
 @endif
+
 
 <script>
     function closeWelcome() {
