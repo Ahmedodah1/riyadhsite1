5d0b8f063ae7d782c4d709e6a9f26d7f3397735e
ahmed <ahmedaoudah1@gmail.com>
2025-08-24 10:45:35 +0300

    qw

--- app/Actions/Warehouses/Admin/WareEditView.php
+++ app/Actions/Warehouses/Admin/WareEditView.php
@@ -12,7 +12,7 @@ class WareEditView
 
     public function handle($id)
     {
-        $warehouse = Warehouses::findOrFail($id);
-        return view('warehouses.admin.edit', compact('warehouse'));
+        $warehouses = Warehouses::findOrFail($id);
+        return view('warehouses.admin.edit', compact('warehouses'));
     }
 }
