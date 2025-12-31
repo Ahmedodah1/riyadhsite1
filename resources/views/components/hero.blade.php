6c3c7b594f81eb7cd2ca6fc9bddc2fb8cee10244
ahmed <ahmedaoudah1@gmail.com>
2026-01-01 02:25:09 +0300

    update

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -123,7 +123,6 @@
 
                 <!-- اسم الشخص -->
                 <p class="text-6xl md:text-4xl font-medium text-center animate-fade-in-up" style="color:#FFC107;">
-                    م. رياض المقيد
                 </p>
 
 
